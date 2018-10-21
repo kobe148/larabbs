7ac91cb077c76e5aef81b4a02ae636019d51fa07
kobe148 <1400102825@qq.com>
2018-10-21 13:08:34 +0000

    添加队列

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -45,4 +45,9 @@ class Topic extends Model
     {
         return $query->orderBy('created_at', 'desc');
     }
+
+    public function link($param = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $param));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
