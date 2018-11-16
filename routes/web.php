f7d056b550f73144636f26dfd03c74b7ea00fbeb
kobe148 <1400102825@qq.com>
2018-11-16 14:26:08 +0000

    后台权限

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
